0bf8b513c1577a687d174cfa629f1a48b4d0a375
hamzoox722 <60244310+hamzoox722@users.noreply.github.com>
2020-02-06 20:51:56 -0500

    Commit 10 (broken)

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -13,6 +13,20 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3d.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collideWith = coll.gameObject;
+        if (collideWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
     }
 }
